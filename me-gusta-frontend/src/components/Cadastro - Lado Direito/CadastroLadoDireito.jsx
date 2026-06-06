c214ccfe81c49789676d594f73df2b3d5eae13db
HenryCaetano <henry.silva@sptech.school>
2026-06-06 02:18:37 -0300

    Alterações: adição do botão de sair do dash e inserção da logo nas abas de cadastro e login

--- me-gusta-frontend/src/components/Cadastro - Lado Direito/CadastroLadoDireito.jsx
+++ me-gusta-frontend/src/components/Cadastro - Lado Direito/CadastroLadoDireito.jsx
@@ -1,8 +1,10 @@
 import './CadastroLadoDireito.css'
+import logoMegusta from '../../assets/logo-megusta.png'
 
 export default function CadastroLadoDireito() {
   return (
     <div className="cartao-direito">
+      <img className="logo-megusta-cadastro" src={logoMegusta} alt="logo-megusta" />
       <div className="pizza"></div>
       <button className="botao-enviar" type="submit" form="register-form">
         Cadastre-se
--- me-gusta-frontend/src/components/Login - Lado Direito/LoginLadoDireito.jsx
+++ me-gusta-frontend/src/components/Login - Lado Direito/LoginLadoDireito.jsx
@@ -1,8 +1,10 @@
 import './LoginLadoDireito.css'
+import logoMegusta from '../../assets/logo-megusta.png'
 
 export default function LoginLadoDireito({ onNavigarDashboard }) {
   return (
     <div className="cartao-direito">
+      <img className="logo-megusta-login" src={logoMegusta} alt="logo-megusta" />
       <div className="pizza"></div>
       <button className="botao-enviar" type="submit" form="login-form" onClick={onNavigarDashboard}>
         Entrar
